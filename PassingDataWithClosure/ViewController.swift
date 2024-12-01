7a20c72979c8618ea5ab468b43d4cf6147d32d67
Blackcode2 <doky03115@gmail.com>
2024-12-01 23:09:56 +0900

    Initial Commit

---
+++ PassingDataWithClosure/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PassingDataWithClosure
+//
+//  Created by KimDogyung on 12/1/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ PassingDataWithClosureTests/PassingDataWithClosureTests.swift
@@ -0,0 +1,17 @@
+//
+//  PassingDataWithClosureTests.swift
+//  PassingDataWithClosureTests
+//
+//  Created by KimDogyung on 12/1/24.
+//
+
+import Testing
+@testable import PassingDataWithClosure
+
+struct PassingDataWithClosureTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
